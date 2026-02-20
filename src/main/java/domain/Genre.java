7166de73fe785b62fd53244e8df20a480efcf019
M3RSI <fisayomimercy@gmail.com>
2026-02-20 16:35:38 -0600

    genre and status

--- src/main/java/domain/Genre.java
+++ src/main/java/domain/Genre.java
@@ -1,23 +1,10 @@
-import com.google.common.base.Preconditions;
-
-public class Genre {
-
-    private ArrayList<Book> listOfBook = new ArrayList<Book>();
-    private GenreType type;
-
-    public Genre(GenreType type){
-        this.type = type;
-    }
-
-    public void addBook(Book b){
-        //add book to list by genre
-        Preconditions.checkNotNull(b, "Book cannot be null");
-
-        if(b.getGenre() == this.type){
-            listOfBook.add(b);
-        }else{
-            throw new InvalidGenreTypeException();
-        }
-    }
-
+public enum GenreType {
+    ROMANCE,
+    THRILLER,
+    MYSTERY,
+    CRIME,
+    HORROR,
+    SELF_HELP,
+    FANTACY,
+    SCI-FI
 }
