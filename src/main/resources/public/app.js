af7bea5bb416ad9ef5402907deac07449396d1b5
M3RSI <fisayomimercy@gmail.com>
2026-02-21 12:21:15 -0600

    frontend done

--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -1,7 +1,6 @@
 //add book
 let allBooks = [];
 let showingBooks = false;
-
 document.getElementById("add_book_form").addEventListener("submit", function(e){
 
     e.preventDefault();
@@ -25,7 +24,12 @@ document.getElementById("add_book_form").addEventListener("submit", function(e){
 document.getElementById("all-books").addEventListener("click", function(){
 
     let container = document.getElementById("book_list");
-    container.innerHTML ="";
+    //container.innerHTML ="";
+    if (showingBooks) {
+        container.innerHTML = "";
+        showingBooks = false;
+        return;
+    }
 
 
     allBooks.forEach(book => {
@@ -44,8 +48,76 @@ document.getElementById("all-books").addEventListener("click", function(){
 
         container.appendChild(div);
     });
+    showingBooks = true;
 
 
 });
+
+//by genre
+document.getElementById("bygenre").addEventListener("change", function(){
+
+    let selectedGenre = this.value;
+    let container = document.getElementById("book_list");
+
+    container.innerHTML ="";
+
+    if(selectedGenre === ""){
+        return;
+    }
+
+    let filtered = allBooks.filter(book => book.genre === selectedGenre);
+
+    //display
+    filtered.forEach(book => {
+        let div = document.createElement("div");
+        div.classList.add("book-item");
+
+        div.innerHTML = `
+            <h3>${book.title}</h3>
+            <p><strong>Author: </strong> ${book.author}</p>
+            <p><strong>ISBN: </strong> ${book.isbn}</p>
+            <p><strong>Pages: </strong> ${book.pages}</p>
+            <p><strong>Rating: </strong> ${book.rating}</p>
+            <p><strong>Genre: </strong> ${book.genre}</p>
+            <p><strong>Status: </strong> ${book.status}</p>
+        `;
+
+        container.appendChild(div);
+    });
+});
+
+//by rating
+document.getElementById("byrating").addEventListener("change", function(){
+
+    let selectedRating = parseInt(this.value);
+    let container = document.getElementById("book_list");
+
+    container.innerHTML ="";
+
+    if(isNaN(selectedRating)){
+        return;
+    }
+
+    let filtered = allBooks.filter(book => book.rating === selectedRating);
+
+    //display
+    filtered.forEach(book => {
+        let div = document.createElement("div");
+        div.classList.add("book-item");
+
+        div.innerHTML = `
+            <h3>${book.title}</h3>
+            <p><strong>Author: </strong> ${book.author}</p>
+            <p><strong>ISBN: </strong> ${book.isbn}</p>
+            <p><strong>Pages: </strong> ${book.pages}</p>
+            <p><strong>Rating: </strong> ${book.rating}</p>
+            <p><strong>Genre: </strong> ${book.genre}</p>
+            <p><strong>Status: </strong> ${book.status}</p>
+        `;
+
+        container.appendChild(div);
+    });
+});
+
 
 
